a0f90f7e25181fc376175c2fa53aa71ec0cb8f52
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-07-19 19:00:16 +0530

    "Added jsonwebtoken dependency and implemented login functionality in Auth controller"

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../model/User");
-
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 //signup route handler
 exports.signup = async (req, res) => {
     try{
@@ -39,6 +40,63 @@ exports.signup = async (req, res) => {
         return res.status(500).json({
             success:false,
             message: "Something went wrong",
+        })
+    }
+}
+
+//login
+exports.login = async (req, res) => {
+    try{
+        //get data
+        const {email, password} = req.body;
+        //validation onemail and password
+        if(!email || !password){
+            return res.status(400).json({message: "Please provide email and password",
+                success:false,
+                });
+        }
+        //check registered user
+        const user = await User.findOne({email});
+        //if no registered user
+        if(!user){
+            return res.status(400).json({message: "User not registered",
+                success:false,
+            });
+        }
+        const payload = {
+            email:user.email,
+            id:user._id,
+            role:user.role,
+        }
+        //verify password and genrate a jwt token
+        if(await bcrypt.compare(password,user.password)){
+            let token = jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:"1d"});
+            user.token=token
+            user.password=undefined;
+            const options = {
+                expires: new Date(Date.now() + 1000 * 60 * 60 *
+                24 * 7),
+                httpOnly:true,
+            }
+            res.cookie("token",token,options).status(200)
+            .json({
+                success:true,
+                token,
+                user,
+                message:"User Logged in successfully",
+            });
+        }
+        else{
+            return res.status(400).json({message: "Invalid credentials",
+                success:false,
+            });
+        }
+    }
+    catch(error){
+        consol.error(error);
+        return res.status(500).json({
+            success:false,
+            message:"Internal server error",
         })
     }
 }
